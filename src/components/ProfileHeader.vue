50366ee8f866c04d12632747124e7cfa9bac5df2
Ziad Yahya <ziadyahya@proton.me>
2026-08-19 21:01:11 +0300

    Update Twitter link in ProfileHeader component

--- src/components/ProfileHeader.vue
+++ src/components/ProfileHeader.vue
@@ -15,7 +15,7 @@
     </p>
 
     <nav aria-label="Social Profiles" class="flex space-x-5 text-xl text-gray-800 mb-10">
-      <a aria-label="Twitter" class="hover:text-gray-600 transition-colors" href="https://x.com/ziad_labs"><i class="fa-brands fa-x-twitter"></i></a>
+      <a aria-label="Twitter" class="hover:text-gray-600 transition-colors" href="https://x.com/ziadlabs"><i class="fa-brands fa-x-twitter"></i></a>
       <a aria-label="GitHub" class="hover:text-gray-600 transition-colors" href="https://github.com/zyahya"><i class="fa-brands fa-github"></i></a>
       <a aria-label="LinkedIn" class="hover:text-gray-600 transition-colors" href="https://linkedin.com/in/zyahya"><i class="fa-brands fa-linkedin"></i></a>
       <a aria-label="Email" class="hover:text-gray-600 transition-colors" href="mailto:contact@zyahya.dev"><i class="fa-solid fa-envelope"></i></a>
